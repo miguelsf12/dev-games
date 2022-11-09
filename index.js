02e9382afabb4aff54370c9071c3c4b5fb1d84b8
miguelsf12 <87654640+miguelsf12@users.noreply.github.com>
2022-11-09 19:52:03 -0300

    add pagina de login

---
+++ index.js
@@ -0,0 +1,15 @@
+function mudouTamanho() {
+    if (window.innerWidth >= 700) {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
+
+function clickMenu() {
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+    }
+}
